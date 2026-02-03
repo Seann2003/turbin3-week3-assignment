912152809a1bac3fc3bf8fad98ec2ead6e4857db
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2026-02-04 01:03:13 +0800

    Create deposit and init instructions for amm

--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -15,7 +15,16 @@ declare_id!("GSjGv62gfZeuw7muY5pf9r2jATPNbcBCoeQUxeqqzg5W");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.initialize(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
     }
 }
